7f2da91383fac73f425400572b756bd0fa9d8426
Jackie Lam <jackielin1992@gmail.com>
2014-06-11 21:35:16 +0800

    Speed up the calculation

--- main.cpp
+++ main.cpp
@@ -12,17 +12,21 @@
 #include <fstream>
 #include <sstream>
 #include <stdlib.h>
+#include <ctime>
+#include <cstdlib>
+#include <iomanip>
 using namespace std;
 
 #define NUM_USER 24983
 #define NUM_MOVIE 101
-#define NUM_CATEGORIES 5
-#define NUM_ITERATION 10000
+#define NUM_CATEGORIES 150
+#define NUM_ITERATION 2000
 
 double ratings[NUM_MOVIE][NUM_USER] = {0};
+double h[NUM_MOVIE][NUM_USER];
 double x[NUM_MOVIE][NUM_CATEGORIES] = {1.0};      // x0 == 1, x1~x5
 double theta[NUM_USER][NUM_CATEGORIES+1] = {1.0};  // theta0~theta5
-double lummda;
+double lamda;
 double alpha;
 
 #pragma mark - Read file
@@ -80,55 +84,80 @@ void readfile(string path, double target[][NUM_USER])
 
 #pragma mark - Calculation
 
-double theta_multi_x(int j, int i)
-{
-    double result = theta[j][0];
-    for (int cnt = 0; cnt < NUM_CATEGORIES; cnt++)
-        result += theta[j][cnt+1]*x[i][cnt];
-    
-    return result;
-}
-
-double Jtheta(int k, double itself, bool isX)
+double calVariance()
 {
     int i, j;
-    double result = 0.0f;
-    double increment;
-    for (j = 0; j < NUM_USER; j++) {
-        for (i = 0; i < NUM_MOVIE; i++) {
-            // judge whether already rated
+    double cnt = 0;
+    double var = 0;
+    for (i = 0; i < NUM_MOVIE; i++) {
+        for (j = 0; j < NUM_USER; j++) {
             if (ratings[i][j] != 0) {
-                if (isX)
-                    increment = (theta_multi_x(j, i) - ratings[i][j]) * theta[j][k];
-                else
-                    increment = (theta_multi_x(j, i) - ratings[i][j]) * x[i][k];
-                result += increment;
+                cnt++;
+                var += (h[i][j] - ratings[i][j]) * (h[i][j] - ratings[i][j]);
             }
         }
     }
-    result += lummda * itself;
-    return result;
+    var = var / cnt;
+    return var;
 }
 
-void calculate()
+void refreshH()
 {
     int i, j, k;
-    for (j = 0; j < NUM_USER; j++) {
-        for (k = 0; k < NUM_CATEGORIES; k++) {
-            // theta.k(j)
-            double tmp = alpha * Jtheta(k, theta[j][k], false);
-            cout << "theta - " << tmp << endl;
-            theta[j][k] = theta[j][k] - tmp;
-        }
-        for (i = 0; i < NUM_MOVIE; i++) {
-            for (k = 0; k <= NUM_CATEGORIES; k++) {
-                // x.k(i)
-                double tmp = alpha * Jtheta(k, x[i][k], true);
-                cout << "x - " << tmp << endl;
-                x[i][k] = x[i][k] - tmp;
-            }
+    for (i = 0; i < NUM_MOVIE; i++) {
+        for (j = 0; j < NUM_USER; j++) {
+            h[i][j] = theta[j][0];
+            for (k = 0; k < NUM_CATEGORIES; k++)
+                h[i][j] += theta[j][k+1]*x[i][k];
         }
     }
+}
+
+void calculate(int times)
+{
+    int i, j, k;
+    time_t start_t, end_t;
+    double zeroVar = calVariance();
+    cout << "[zeroVar] " << zeroVar << endl;
+    
+    start_t = time(NULL);
+    
+    // 2. calculate theta
+    for (j = 0; j < NUM_USER; j++) {
+        for (k = 0; k < NUM_CATEGORIES+1; k++) {
+            int sum = 0;
+            for (i = 0; i < NUM_MOVIE; i++) {
+                if (ratings[i][j] != 0)
+                    sum += (h[i][j] - ratings[i][j]) * x[i][k];
+            }
+            theta[j][k] = theta[j][k] - alpha * (sum + lamda * theta[j][k]);
+        }
+    }
+    refreshH();
+    double firstVar = calVariance();
+    
+    // 1.calculate x
+    for (i = 0; i < NUM_MOVIE; i++) {
+        for (k = 0; k < NUM_CATEGORIES; k++) {
+            int sum = 0;
+            for (j = 0; j < NUM_USER; j++) {
+                if (ratings[i][j] != 0)
+                    sum += (h[i][j] - ratings[i][j]) * theta[j][k];
+            }
+            x[i][k] = x[i][k] - alpha * (sum + lamda * x[i][k]);
+        }
+        refreshH();
+    }
+    
+    
+    end_t = time(NULL);
+    
+    
+    double secondVar = calVariance();
+    cout << "[Time used] " << difftime(end_t, start_t) << endl;
+    cout << "[firstVar] " << firstVar << endl;
+    cout << "[secondVar] " << secondVar << endl;
+    cout << "[firstVar - secondVar] " << firstVar - secondVar << endl;
 }
 
 #pragma mark - Testing
@@ -156,8 +185,10 @@ void writeforeachrow(ofstream &outfile, const string& src, const string& delim)
     outfile << item_id << ",";
     int item = atoi(item_id.c_str());
     
-    double result;
-    result = theta_multi_x(item-1, user-1);
+    double result = theta[item-1][0];
+    for (int k = 0; k < NUM_CATEGORIES; k++)
+        result += theta[item-1][k+1]*x[user-1][k];
+    
     stringstream convert;
     convert << result;
     outfile << convert.str();
@@ -190,23 +221,29 @@ void testandwrite(string readpath, string writepath)
 
 int main()
 {
+    srand(time(NULL));
     for (int i = 0; i < NUM_MOVIE; i++)
-        for (int k = 0; k < NUM_CATEGORIES; k++)
-            x[i][k] = 0.001;
+        for (int k = 0; k < NUM_CATEGORIES; k++) {
+            x[i][k] = (double)(rand() % 10) / 100.0f;
+        }
     
     for (int j = 0; j < NUM_USER; j++)
-        for (int k = 0; k <= NUM_CATEGORIES+1; k++)
-            theta[j][k] = 0.001;
-    lummda = 0.02;
-    alpha = 0.00001;
-    readfile("/Users/apple/Developer/Recommender/Recommender/train.txt", ratings);
-    //    for (int cnt = 0; cnt < NUM_ITERATION; cnt++) {
-    //        cout << "Times - " << cnt << endl;
-    calculate();
-    //    }
+        for (int k = 0; k <= NUM_CATEGORIES+1; k++) {
+            theta[j][k] = (double)(rand() % 10) / 100.0f;
+        }
     
+    refreshH();
+    lamda = 100;
+    alpha = 0.0001;
+    readfile("/Users/apple/Developer/Recommender/Recommender/train.txt", ratings);
+    cout << "adsf" << endl;
+    for (int cnt = 1; cnt <= NUM_ITERATION; cnt++) {
+        cout << "TIMES - " << cnt << endl;
+        calculate(cnt);
+    }
     testandwrite("/Users/apple/Developer/Recommender/Recommender/test.txt", "/Users/apple/Developer/Recommender/Recommender/result.csv");
     
     
     return 0;
 }
+
